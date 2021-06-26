43f506f28be85d17d627c2aa9549d844b7aeedec
viktormishyn <viktormishyn@gmail.com>
2021-06-26 14:00:29 +0300

    Modify handler.js and serverless.yml (substitute hello func with createOrder); create /order endpoint

--- nodejs/handler.js
+++ nodejs/handler.js
@@ -1,11 +1,11 @@
 "use strict";
 
-module.exports.hello = async (event) => {
+module.exports.createOrder = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v1.0! Your function executed successfully!",
+        message: "Order created!",
         input: event,
       },
       null,
